7cb86871109be0229939d3aa3c0173227cae5981
Navarro Rosa <navarro.msrosa@gmail.com>
2019-05-13 15:39:01 -0300

    export function

--- R/misc_date.R
+++ R/misc_date.R
@@ -22,6 +22,7 @@ insert_zero_month <- function(x, divisor = "/"){
 #' @import stringr data.table
 #'
 #' @return Nada. Altera INPLACE a coluna do \code{data.table} especificada
+#'
 #' @export
 
 misc_date <- function(dados,
