39bd18130337fca111da420072c22cbeeeab5aad
Navarro Rosa <nrosa@funcionalcorp.com.br>
2019-07-03 16:34:04 -0300

    fix misssing function

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+#' magikaRp
+#'
+#' Funções úteis a mim
+#'
+#' @docType package
+#' @author Navarro Mendes Santos Rosa
+#' @import     data.table xgboost magrittr mlrMBO  smoof DiceKriging rgenoud lhs ParamHelpers Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib magikaRp
+#' @name magikaRp
+NULL
